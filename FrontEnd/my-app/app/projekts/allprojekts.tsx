54bb63cd339e5d77928f377105dfc031e70e8173
lobna3 <yousfi55lobna@gmail.com>
2024-07-12 19:59:52 +0200

    update

--- FrontEnd/my-app/app/projekts/allprojekts.tsx
+++ FrontEnd/my-app/app/projekts/allprojekts.tsx
@@ -26,14 +26,14 @@ const AllProjk = () => {
             })
     }
 
-    const deleteProjekt = (id: any) => {
+    const deleteProjekt = (id: string) => {
         axios.delete(`http://127.0.0.1:5000/api/projekts/${id}`).then((response) => {
             console.log('Projekt deleted successfully', response.data)
             setRefetsch(!refetsch)
         }).catch((error) => { console.log(error) })
     }
 
-    const searchProjekt = (title: any) => {
+    const searchProjekt = (title: string) => {
         axios.get(`http://127.0.0.1:5000/api/projekts/title/${title}`).then((response) => {
             console.log(response.data)
             setProjekts(response.data)
@@ -173,7 +173,7 @@ const AllProjk = () => {
                                                 </button>
                                                 <button type="button" id="updateProductButton" data-drawer-target="drawer-update-product-default" data-drawer-show="drawer-update-product-default" aria-controls="drawer-update-product-default" data-drawer-placement="right" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                                                     <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path><path fillRule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path></svg>
-                                                    Update
+                                                    <Link href={`projekts/updateProjekt/${ele.id}`}>Update</Link>
                                                 </button>
                                                 <button onClick={() => { setOpen(true), setId(ele.id) }}type="button" id="deleteProductButton" data-drawer-target="drawer-delete-product-default" data-drawer-show="drawer-delete-product-default" aria-controls="drawer-delete-product-default" data-drawer-placement="right" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900">
                                                     <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path></svg>
@@ -182,8 +182,6 @@ const AllProjk = () => {
                                             </td>
                                         </tr>
                                     )}
-
-
                                 </tbody>
                             </table>
                         </div>
---
+++ FrontEnd/my-app/app/projekts/updateProjekt/[slug]/page.tsx
@@ -0,0 +1,161 @@
+'use client'
+
+import React, { useState, useEffect } from "react"
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
+import Link from "next/link"
+
+interface Project {
+    title: string;
+    category: string;
+    evaluation: string;
+    createdAt: string;
+    description: string;
+    imageUrl: string
+}
+
+const initialProjectState: Project = {
+    title: "",
+    category: "",
+    evaluation: "",
+    createdAt: "",
+    description: "",
+    imageUrl: ""
+
+}
+
+export default function UpdateProjekt({ params }: { params: { slug: string } }) {
+
+    const [projekt, setProjekt] = useState<Project>(initialProjectState);
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+    const [imageUrl, setImageUrl] = useState("")
+    const [category, setCategory] = useState("")
+    const [evaluation, setEvaluation] = useState("")
+    const router = useRouter()
+
+
+    const updateProjekt = (id: string) => {
+        axios.put(`http://127.0.0.1:5000/api/projekts/${id}`).then((response) => {
+            console.log('Projekt updated successfully', response.data)
+            router.push('/projekts')
+        }).catch((error) => { console.log(error) })
+    }
+    
+    const fetchProject = async (id: string) => {
+        try {
+            const response = await axios.get(`http://127.0.0.1:5000/api/projekts/getAll/${id}`);
+            console.log(response.data)
+            setProjekt(response.data);
+            setTitle(response.data.title)
+            setDescription(response.data.description)
+            setCategory(response.data.category)
+            setEvaluation(response.data.evaluation)
+            setImageUrl(response.data.imageUrl)
+        } catch (error) {
+            console.error('Error fetching project:', error);
+        }
+    }
+
+
+
+    useEffect(() => {
+        if (params.slug) {
+            fetchProject(params.slug);
+        }
+    }, [params.slug]);
+
+    return (
+        <div>
+            <div className="grid grid-cols-1 px-4 pt-6 xl:grid-cols-3 xl:px-0 xl:gap-4 dark:bg-gray-900">
+                <div className="mb-4 col-span-full xl:mb-2">
+                    <nav className="flex mb-5" aria-label="Breadcrumb">
+                        <ol className="inline-flex items-center space-x-1 text-sm font-medium md:space-x-2">
+                            <li className="inline-flex items-center">
+                                <Link href="/" className="inline-flex items-center text-gray-700 hover:text-primary-600 dark:text-gray-300 dark:hover:text-primary-500">
+                                    <svg className="w-5 h-5 mr-2.5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
+                                    Home
+                                </Link>
+                            </li>
+                            <li>
+                                <div className="flex items-center">
+                                    <svg className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                                    <Link href="/projekts" className="ml-1 text-gray-700 hover:text-primary-600 md:ml-2 dark:text-gray-300 dark:hover:text-primary-500">Projekts</Link>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="flex items-center">
+                                    <svg className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                                    <Link href="/" className="ml-1 text-gray-400 md:ml-2 dark:text-gray-500" aria-current="page">Tasks</Link>
+                                </div>
+                            </li>
+                        </ol>
+                    </nav>
+                    <h1 className="text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">My Projekt: {params.slug}</h1>
+                </div>
+            </div>
+            <div className='flex flex-col'>
+                <div className="max-w-md mx-auto bg-white p-8 rounded-lg ">
+
+                    <div className="mb-4">
+                        <div className="relative">
+                            <label className="block text-gray-700 text-sm font-bold mb-2">Title:</label>
+                            <input type="text" className=" outline-none appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder=" Your Title"
+                                value={title}
+                                onChange={(e) => { setTitle(e.target.value) }} />
+                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                <i className="fas fa-user text-gray-400"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mb-4">
+                        <div className="relative">
+                            <label className="block text-gray-700 text-sm font-bold mb-2">Description:</label>
+                            <textarea className=" outline-none appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder=" Your Description" value={description}
+                                onChange={(e) => { setDescription(e.target.value) }} />
+                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                <i className="fas fa-user text-gray-400"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mb-4">
+                        <div className="relative">
+                            <label className="block text-gray-700 text-sm font-bold mb-2"> Choose a Category:</label>
+                            <select onChange={(e) => { setCategory(e.target.value) }} className=" outline-none appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+                                <option value="It" >It</option>
+                                <option value="Managments" >Managments</option>
+                                <option value="Marketing" >Marketing</option>
+                            </select>
+                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                <i className="fas fa-user text-gray-400"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mb-4">
+                        <div className="relative">
+                            <label className="block text-gray-700 text-sm font-bold mb-2">Evaluation:</label>
+                            <input type="text" className=" outline-none appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Your Evaluation"
+                                value={evaluation} onChange={(e) => { setEvaluation(e.target.value) }}
+                            />
+                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                <i className="fas fa-user text-gray-400"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2">Upload Image:</label>
+                        <img src={imageUrl} className='w-14 mt-6' />
+                        <input type="file" className="outline-none appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" onChange={(e) => setImageUrl(e.target.value)} />
+                    </div>
+
+                    <button className=" ml-28 mt-8 bg-blue-800 hover:bg-blue text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" value="submit" onClick={() => {
+                        updateProjekt(params.slug)
+                    }}>Update Projekt</button>
+                </div>
+
+
+            </div>
+
+        </div>
+    )
+}
