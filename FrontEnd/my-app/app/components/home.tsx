50ca5c3dc6874805e37bc065e869c72641a92c11
lobna3 <yousfi55lobna@gmail.com>
2024-07-13 17:50:34 +0200

    Function to handle logout

--- FrontEnd/my-app/app/components/home.tsx
+++ FrontEnd/my-app/app/components/home.tsx
@@ -1,11 +1,12 @@
 'use client'
 
-import React , {useState,useEffect} from "react"
+import React, { useState, useEffect } from "react"
 import Link from "next/link"
 import { jwtDecode } from "jwt-decode";
+import Nav from "./Nav";
 
 interface User {
-    id:string
+    id: string
     name: string;
     email: string;
     role: string;
@@ -25,7 +26,7 @@ const HomePage = () => {
     useEffect(() => {
         const token = localStorage.getItem('token');
         const isAuthenticated = localStorage.getItem('isAuthenticated');
-        
+
         // Check if token and isAuthenticated are set
         if (token && isAuthenticated) {
             const decodedToken: any = jwtDecode(token);
@@ -53,22 +54,30 @@ const HomePage = () => {
                         ) : (
                             <Link href="/login" className="text-sm font-semibold leading-6 text-gray-900">
                                 Log in <span aria-hidden="true">&rarr;</span>
-                            </Link>
+                            </Link>   
                         )}
                     </div>
                 </nav>
             </header>
 
-            {/* Rest of your homepage content */}
             <div className="relative isolate px-6 pt-14 lg:px-8">
-                {/* Your main content sections */}
-                {/* Example content */}
-                <div className="text-center">
-                    <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Welcome To Projekt Managment</h1>
+                <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+                    <div className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" ></div>
+                </div>
+                <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+
+                    <div className="text-center">
+                        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Welcome To Projekt Managment</h1>
+
+                    </div>
+                </div>
+                <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
+                    <div className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" ></div>
                 </div>
             </div>
         </div>
-    );
+
+    )
 }
 
-export default HomePage;
+export default HomePage
